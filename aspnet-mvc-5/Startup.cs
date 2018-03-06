b47e002f6e4b711b29fd65bc79074a1a07fb4d97
Canh Doan <canh.doanthanh@asnet.com.vn>
2018-03-06 17:24:35 +0700

    Updates project

---
+++ aspnet-mvc-5/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(aspnet_mvc_5.Startup))]
+namespace aspnet_mvc_5
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
